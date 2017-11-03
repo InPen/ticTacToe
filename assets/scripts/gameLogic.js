e51d3c0d0c9b4df2468047848404e5f0abbe1979
maria.inespeniche-vargas@generalassemb.ly <maria.inespeniche-vargas@generalassemb.ly>
2017-11-03 10:41:38 -0400

    Add pseudo game logic
    Add pseudo code to begin functions

---
+++ assets/scripts/gameLogic.js
@@ -0,0 +1,42 @@
+const str = []
+const x = concha
+const o = churro
+// choosing a cell pseudo code
+
+
+// navigating in the array and cell coordinates
+.lenght
+// x clicks on cell
+.on
+// cell goes from blank to display x
+if concha === true
+console.log('x')
+// turn switches
+array refreshes but knows of the occupied cell
+// o clicks on cell
+.on
+// cell changes from blank to display o
+else if console.log('o')
+// turn switches
+array refreshes but knows of the occupied cell
+// if user clicks on an occupied cell
+else console.log("don't be shellFish! That cell has already been used :P")
+
+
+// winning x | o code
+
+
+// if x wins
+if 3 cells in a row match x
+then x wins
+console.log('Concha wins!')
+
+// if o wins
+if 3 cells in a row match o
+then o wins
+console.log('Churros wins!')
+
+// else if draw
+if no cells match in a row
+then its a draw
+console.log('Nadie wins :/')
