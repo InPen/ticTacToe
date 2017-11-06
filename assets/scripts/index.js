08e6ed687a0b317fc48a756b640af1ae2a0a2346
maria.inespeniche-vargas@generalassemb.ly <maria.inespeniche-vargas@generalassemb.ly>
2017-11-06 09:51:25 -0500

    Recover code
    Implement code previously built that allows me to display content in my
    cell and switch between player.
    Re-starting game logic for wins

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,46 +1,41 @@
 'use strict'
 
-const setAPIorigin = require('../../lib/set-api-origin')
+const setAPIOrigin = require('../../lib/set-api-origin')
 const config = require('./config')
 // const events = require('./auth/events')
 // const gameLogic = require('assets/scripts/gameLogic')
 $(() => {
-  setAPIorigin(location, config)
+  setAPIOrigin(location, config)
 })
-let currentPlayer = ''
+let currentPlayer = 'x'
 
-$('.bo').click(function (event) {
-  if ($(event.target).tet() === '') {
-    $(event.target).tet(currentPlayer)
-    switchPlayer()
-  }
+$('.box').click(function (event) {
+  $(event.target).text()
+  console.log($(event.target).text())
+  $(event.target).text(currentPlayer)
+  switchPlayer()
 })
 
-// target cell we click on to change tet
+// target cell we click on to change text
 
 function switchPlayer () {
-  if (currentPlayer === '') {
+  if (currentPlayer === 'x') {
+    console.log('hi')
     currentPlayer = 'o'
+    console.log('bye')
   } else {
-    currentPlayer = ''
+    currentPlayer = 'x'
   }
 }
 
-// win logic
-
-const checkWins = function (cell) {
-  // cell is not an empty string
-  if ((cell[0] === '' && cell[1] === '' && cell[2] === '') ||
-  (cell[3] === '' && cell[4] === '' && cell[5] === '') ||
-  (cell[6] === '' && cell[7] === '' && cell[8] === '') ||
-  (cell[0] === '' && cell[3] === '' && cell[6] === '') ||
-  (cell[1] === '' && cell[4] === '' && cell[7] === '') ||
-  (cell[2] === '' && cell[5] === '' && cell[8] === '') ||
-  (cell[0] === '' && cell[4] === '' && cell[8] === '') ||
-  (cell[2] === '' && cell[4] === '' && cell[6] === '')) {
-    return true
-  }
-}
+// invoke function everytime the user clicks
+// function that switches between x and o
+// $( event.target );
+//   if ( target.is( "li" ) ) {
+//     target.children().toggle();
+//   }
+// }
+// $(() => {
 //   events.addHandlers()
 // })
 
@@ -48,7 +43,7 @@ const checkWins = function (cell) {
 //   gameLogic.addHandlers()
 // })
 // use require with a reference to bundle the file and use it in this file
-// const eample = require('./eample')
+// const example = require('./example')
 
 // use require without a reference to ensure a file is bundled
-// require('./eample')
+// require('./example')
