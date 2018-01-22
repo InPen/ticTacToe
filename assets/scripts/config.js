9f93f8f77cf8f8b96f191cae7b9aa72e84ed281a
maria.inespeniche-vargas@generalassemb.ly <maria.inespeniche-vargas@generalassemb.ly>
2018-01-22 16:23:18 -0500

    Change API route

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 const config = {
   apiOrigins: {
-    production: 'https://tic-tac-toe.wdibos.com',
+    production: 'https://aqueous-atoll-85096.herokuapp.com',
     development: 'https://aqueous-atoll-85096.herokuapp.com'
   }
 }
